906116994a8ce95b1686a4b7595f83b26dd973f1
tikhonmakeev <tihon.makeev@yandex.ru>
2024-11-28 15:44:54 +0300

    corrected init file

--- backend/parsers/__init__.py
+++ backend/parsers/__init__.py
@@ -1,1 +1,5 @@
+from parsers.db_parser import db_parser
+from parsers.site_parser import site_parser
+from parsers.table_parser import table_parser
+
 __all__ = ["db_parser", "site_parser", "table_parser"]
